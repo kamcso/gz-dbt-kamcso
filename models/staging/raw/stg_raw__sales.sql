a1da69ec09bbb356fa4a6b8f893e6b6f2da154e9
Kamilla <radeczkikamilla@gmail.com>
2024-12-09 15:01:40 +0000

    new name

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -2,7 +2,7 @@ with
 
     source as (select * from {{ source("raw", "sales") }}),
 
-    renamed as (select date_date, orders_id, products_id, revenue, quantity from source)
+    renamed as (select date_date, orders_id, pdt_id AS products_id, revenue, quantity from source)
 
 select *
 from renamed
